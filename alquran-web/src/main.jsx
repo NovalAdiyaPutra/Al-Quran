4d0a820e6a3d24ac643ed2cc792a29a8952679e6
NovalAdiyaPutra <12350112933@students.uin-suska.ac.id>
2025-04-13 18:38:49 +0700

    siap publish

--- alquran-web/src/main.jsx
+++ alquran-web/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./arabic.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
